44c6bebfc6a1d873d8b017dd82da997a63b01de2
amdaaraijen <bintangtriadmaja@gmail.com>
2024-11-23 09:00:54 +0700

    feat: create banner on game page

--- src/app/[game]/page.tsx
+++ src/app/[game]/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 interface Props {
@@ -8,8 +9,39 @@ interface Props {
 
 export default function page({ params }: Props) {
   return (
-    <div className="p-20 flex flex-col items-center justify-center gap-5">
-      <h1>{params.game} Page</h1>
+    <div className="flex flex-col items-center justify-center">
+      <div className="relative w-full h-[300px] border-b-2 shadow-sm">
+        <Image
+          src="/banner/banner.png"
+          alt="Xavier"
+          fill
+          className="object-cover"
+          priority
+        />
+      </div>
+      <section className="px-10 w-full">
+        <div className="flex p-5 border border-zinc-500 shadow bg-zinc-300/80 backdrop-blur-sm relative bottom-2 w-full rounded-md">
+          <div className="w-1/6">
+            <div className="absolute border rotate-1 border-zinc-400 bg-zinc-200/50 backdrop-blur-sm w-36 h-52 rounded-md bottom-5 left-10 shadow-sm">
+              <Image
+                src="/xavier.jpg"
+                alt="Xavier"
+                fill
+                className="object-cover rounded-md rotate-6 shadow-md"
+                priority
+              />
+            </div>
+          </div>
+          <div>
+            <h1 className="text-4xl font-light tracking-wider">
+              {params.game}
+            </h1>
+            <p className="text-sm font-light tracking-wider">
+              Fast, secure, and reliable.
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
