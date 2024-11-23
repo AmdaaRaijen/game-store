77e10c07fa022761d916bfc54095c76817631614
amdaaraijen <bintangtriadmaja@gmail.com>
2024-11-23 21:45:04 +0700

    feat: create product page layout

--- src/app/[game]/page.tsx
+++ src/app/[game]/page.tsx
@@ -42,6 +42,31 @@ export default function page({ params }: Props) {
           </div>
         </div>
       </section>
+      <section className="px-10 flex items-start justify-between gap-3">
+        <div className="w-full sticky top-2 h-fit bg-zinc-200 rounded-md p-5 border border-zinc-500 shadow">
+          <div className="prose">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
+            ea qui tempora. Culpa fuga tempore et quis hic, sunt dolorem
+            asperiores illo facere amet aut quibusdam commodi rerum. Dolor vero
+            rem iure deserunt, ullam ea quo tempore! Distinctio ea possimus
+            iusto necessitatibus animi suscipit eligendi, officia qui asperiores
+            nostrum, esse modi accusamus a, magni expedita voluptas accusantium
+            voluptates ut tempore porro dolorum.
+          </div>
+        </div>
+        <div className="w-full min-h-[200vh] bg-zinc-200 rounded-md p-5 border border-zinc-500 shadow">
+          <div className="prose">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At saepe
+            odit, molestiae nemo, ipsa porro provident debitis dolore quia modi
+            similique possimus nihil architecto perspiciatis cum sequi, amet
+            quidem consequuntur! Perferendis ex quia saepe unde voluptatem
+            corrupti, obcaecati, iste error, iusto in rem nesciunt nam a quos
+            facere tempore eos. Ut illo itaque nulla iste architecto dolor,
+            neque dolorum. Delectus magni sapiente minus laboriosam est,
+            blanditiis optio consequuntur voluptates omnis.
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
